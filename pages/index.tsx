ca0a08997fd140ad37cd76acbf65bacedf7faf8d
simba-fs <simba.fs@gmail.com>
2022-01-29 00:24:57 +0800

    test something

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,23 @@
 import type { NextPage } from 'next'
+import { FormEventHandler, useState } from 'react'
+import style from '../styles/container.module.scss'
 
 const Home: NextPage = () => {
-  return (
-	<h1>hello world</h1>
-  )
+	const [text, setText] = useState('')
+
+	const handleSubmit: FormEventHandler = (e) => {
+		e.preventDefault()
+	}
+
+	return (
+		<div className={style.container}>
+			<form action="/test" method="post" onSubmit={handleSubmit}>
+				<h1>Hello Next.js</h1>
+				<input type="text" value={text} onChange={(e) => setText(e.target.value)}/>
+				<button type="submit">Send</button>
+			</form>
+		</div>
+	)
 }
 
 export default Home
